380458ceb8d8391d910cdd4567600a35485a17c8
dave-ck <daveck@protonmail.com>
2018-11-21 12:06:45 +0000

    re-integrated JUnit tests

--- src/GraphFunctionalityTest.java
+++ src/GraphFunctionalityTest.java
@@ -1,7 +1,8 @@
-import static org.junit.Assert.*;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GraphFunctionalityTest {
 	private int targetCity;
