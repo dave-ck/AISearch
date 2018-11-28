b355a0b83ade6d46c6d77a840aa509f0cd1cf3f6
dave-ck <daveck@protonmail.com>
2018-11-28 18:50:21 +0000

    tested functionality with random ints - works as intended

---
+++ src/BoundedPriorityQueue.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class BoundedPriorityQueue<E> extends ArrayList<E>{
+	
+	private int bound;
+	private Comparator<E> eComparator;
+	
+	public BoundedPriorityQueue(int bound, Comparator<E> eComparator){
+		super();
+		this.bound = bound;
+		this.eComparator = eComparator;
+	}
+	
+	@Override
+	public boolean add(E e){
+		if (size() < bound){
+			super.add(e);
+			Collections.sort(this, eComparator);
+			return true;
+		}
+		// if e is better than the worst existing element
+		if (eComparator.compare(get(bound-1), e)>0){
+			//remove the "worst" existing element, substitute with e
+			this.remove(bound-1);
+			this.add(e);
+			Collections.sort(this, eComparator);
+			return true;
+		}
+		return false;
+	}
+	
+	public E pop(int index){
+		E result = get(index);
+		remove(index);
+		return result;
+	}
+}
